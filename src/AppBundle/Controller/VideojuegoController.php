968fddbc766f6ec353140488ba5f9cddfc257279
unknown <vicentepalacios98@gmail.com>
2020-05-10 09:30:16 +0200

    Arreglada la obtención del repositorio en el controlador

--- src/AppBundle/Controller/VideojuegoController.php
+++ src/AppBundle/Controller/VideojuegoController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class VideojuegoController extends Controller
 {
     /**
-     * @Route("/videojuegos", name="")
+     * @Route("/videojuegos", name="videojuegos")
      */
     //método para renderizar la portada de la página Videojuegos con todos los videojuegos de la BBDD
     public function portadaAction() 
     {
-        $em = $this->getRepository()->getManager();
+        $em = $this->getDoctrine()->getManager();
         $videojuegos = $em->getRepository("AppBundle:Videojuego")->findAll();
         return $this->render('videojuegos/videojuegos.html.twig', array(
             'videojuegos' => $videojuegos
